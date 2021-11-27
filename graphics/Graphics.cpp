93599eff684b7aac409f69d033a363a9bb475074
ThatCreeper <creeper_host@yahoo.com>
2021-11-26 20:13:08 -0600

    Windowing

--- graphics/Graphics.cpp
+++ graphics/Graphics.cpp
@@ -1,9 +1,38 @@
 #include "Graphics.hpp"
 
-Graphics::Graphics()
+#include <SDL2/SDL.h>
+
+Graphics::Graphics(const char* game_title)
 {
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+	{
+		throw "SDL_Init(...) != 0";
+	}
+
+	window = SDL_CreateWindow(game_title,
+		SDL_WINDOWPOS_CENTERED,
+		SDL_WINDOWPOS_CENTERED,
+		800,
+		600,
+		0);
+
+	SDL_Event event;
+
+	bool running = true;
 }
 
 Graphics::~Graphics()
+{
+	SDL_DestroyWindow(window);
+
+	SDL_Quit();
+}
+
+bool Graphics::getEvent(SDL_Event* event)
+{
+	return SDL_PollEvent(event);
+}
+
+void Graphics::frame()
 {
 }
--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
+#include <SDL2/SDL_main.h>
+#undef main
+
 #include "graphics/Graphics.hpp"
 
 int main()
 {
-	Graphics graphics;
+	Graphics graphics("Quark Engine");
+
+	bool running = true;
+
+	SDL_Event event;
+	while (running)
+	{
+		while (graphics.getEvent(&event))
+		{
+			if (event.type == SDL_QUIT)
+			{
+				running = false;
+			}
+		}
+
+		graphics.frame();
+	}
+
+	return 0;
 }
